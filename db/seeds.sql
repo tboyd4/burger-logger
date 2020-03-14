d72482754bfd30437e5e3d4c8eb0a66a01725813
tboyd4 <tboyd1898@gmail.com>
2020-03-14 17:58:20 -0500

    seed.sql

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("CheezBurger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("HamBurger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("TurkeyBurg", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Lamb Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BBQ Burg Supreme", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burg", false);
