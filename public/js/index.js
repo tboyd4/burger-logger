b4a2f496b86902693f7e54e3697da44f691923f6
tboyd4 <tboyd1898@gmail.com>
2020-03-14 19:14:42 -0500

    functionality of app done

--- public/js/index.js
+++ public/js/index.js
@@ -17,40 +17,23 @@ $(function() {
     });
   });
 
-  //   $(".create-form").on("submit", function(event) {
-  //     // Make sure to preventDefault on a submit event.
-  //     event.preventDefault();
+  $("#submit-btn").on("click", function(event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
 
-  //     var newCat = {
-  //       name: $("#ca")
-  //         .val()
-  //         .trim(),
-  //       sleepy: $("[name=sleepy]:checked")
-  //         .val()
-  //         .trim()
-  //     };
+    var newBurger = {
+      burger_name: $("#new_burger")
+        .val()
+    };
 
-  //     // Send the POST request.
-  //     $.ajax("/api/cats", {
-  //       type: "POST",
-  //       data: newCat
-  //     }).then(function() {
-  //       console.log("created new cat");
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     });
-  //   });
-
-  //   $(".delete-cat").on("click", function(event) {
-  //     var id = $(this).data("id");
-
-  //     // Send the DELETE request.
-  //     $.ajax("/api/cats/" + id, {
-  //       type: "DELETE"
-  //     }).then(function() {
-  //       console.log("deleted cat", id);
-  //       // Reload the page to get the updated list
-  //       location.reload();
-  //     });
-  //   });
+    // Send the POST request.
+    $.ajax("/api/burger", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("created new burger");
+      // Reload the page to get the updated list
+      location.reload();
+    });
+  });
 });
